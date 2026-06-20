81b47a9549454a1dd3339da3c1092cbc0a51a49a
S.L.Transit <thailandjapan0@gmail.com>
2026-06-20 11:27:41 +0700

    Register refreshed icon service worker

--- site-runtime.js
+++ site-runtime.js
@@ -29,7 +29,7 @@
   }
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-      navigator.serviceWorker.register('sw.js?v=20260620icon').catch(function () {});
+      navigator.serviceWorker.register('sw.js?v=20260620b').catch(function () {});
     });
   }
 })();
