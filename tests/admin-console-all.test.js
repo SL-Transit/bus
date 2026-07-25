23ea50991c88221b44fb8420286cc23e9ce4859d
S.L.Transit <283841188+SL-Transit@users.noreply.github.com>
2026-07-25 09:02:27 +0700

    Add admin console aggregate test

---
+++ tests/admin-console-all.test.js
@@ -0,0 +1,5 @@
+require('./admin-console-safety.test');
+require('./admin-console-ui-contract.test');
+require('./admin-console-release-readiness.test');
+
+console.log('admin console all checks ok');
